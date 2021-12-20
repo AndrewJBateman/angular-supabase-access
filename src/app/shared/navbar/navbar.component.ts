a086de00b8284aacb5e20dddb0403b447f6c690c
AndrewJBateman <gomezbateman@yahoo.com>
2021-12-20 13:44:25 +0100

    feat: add home page and navbar

---
+++ src/app/shared/navbar/navbar.component.ts
@@ -0,0 +1,25 @@
+import { Component } from '@angular/core';
+import { AuthService } from '@auth/services/auth.services';
+
+@Component({
+  selector: 'app-navbar',
+  templateUrl: './navbar.component.html',
+  styleUrls: ['./navbar.component.scss']
+})
+export class NavbarComponent {
+
+  // user$ observable used to switch logout template on or off
+  user$ = this.authSvc.user$;
+
+  constructor(private readonly authSvc: AuthService) { }
+
+  // call signout function from auth service
+  async onLogout(): Promise<void> {
+    try {
+      await this.authSvc.signOut();
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+}
